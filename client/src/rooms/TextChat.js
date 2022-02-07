dca67c74a8b898c700fec799c478fb076f18a941
rutvikpansare <rutvik.pansare@gmail.com>
2022-02-07 14:50:48 -0500

    Text Box Modeified

--- client/src/rooms/TextChat.js
+++ client/src/rooms/TextChat.js
@@ -1,9 +1,17 @@
 import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faAlignCenter, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import styled from "styled-components";
+
+const Container = styled.button`
+  display: inline-block;
+  margin-right: 1em;
+  text-align: center;
+  height: 6.5vh
+`;
+
 
 const TextChat = ({onSend}) => {
-
   let [message, setMessage] = useState("");
 
   function handleSendClick() {
@@ -17,16 +25,16 @@ const TextChat = ({onSend}) => {
   function onTyping(e) {
     setMessage(e.target.value)
   }
-
+  
   return(
     <>
       <div style={{display: "flex", alignItems: "center"}}>
-        <textarea rows={2} cols={80} onChange={onTyping} style={{resize: "none"}}></textarea>
+        <textarea  rows={2} cols={80} onChange={onTyping} style={{resize: "none",borderRadius: 30}}></textarea>
         <span style={{padding: "0.5em", lineHeight: "0.5em"}}>
-          <button className="chat-fa-text-chat-icon" onClick = {handleSendClick}>
+          <Container  className="chat-fa-text-chat-icon" onClick = {handleSendClick}>
             <FontAwesomeIcon icon={faPaperPlane} className="chat-fa-icon" size="lg"  />
-            <span className="lead" style={{padding: "0.5em"}}>Send</span>
-          </button>
+            <span className="lead" style={{padding: "0.5 em"}}>Send</span>
+          </Container>
           
         </span>
       </div>

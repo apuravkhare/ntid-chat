6803f7abd6040149082d714c7aca6ad7d4626836
rutvikpansare <rutvik.pansare@gmail.com>
2022-03-02 19:15:49 -0500

    Typo fix in Room.js

--- client/src/rooms/TextChat.js
+++ client/src/rooms/TextChat.js
@@ -1,12 +1,18 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAlignCenter, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import styled from "styled-components";
+import { propTypes } from "react-bootstrap/esm/Image";
 
 
-const TextChat = ({onSend}) => {
+const TextChat = ({onSend,value}) => {
+   
+  useEffect(() => {
+    console.log("use effect log")
+    setInitMessage(value)
+  })
+  let [initMessage, setInitMessage] = useState("")
   let [message, setMessage] = useState("");
-
   function handleSendClick() {
     if (onSend) {
       onSend(message);
@@ -17,11 +23,12 @@ const TextChat = ({onSend}) => {
 
   function onTyping(e) {
     setMessage(e.target.value)
+    console.log("on type")
   }
   
   return(
     <>
-      <input type="text" onChange={onTyping} className="w-75 h-100" style={{resize: "none", borderRadius: 30, fontSize:"x-large"}}></input>
+      <input type="text" value = {initMessage} onChange={onTyping} className="w-75 h-100" style={{resize: "none", borderRadius: 30, fontSize:"x-large"}}></input>
       <span className="chat-fa-text-chat-icon" onClick = {handleSendClick} style={{marginLeft:"0.5em"}}>
         <FontAwesomeIcon icon={faPaperPlane} className="chat-fa-icon" size="lg" />
         <span className="lead" style={{padding: "0.5 em"}}>Send</span>

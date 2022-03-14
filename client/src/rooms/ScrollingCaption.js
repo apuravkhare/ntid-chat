4f8c9808e4909af4c8538d701dbef5d115be8633
Apurav <33133457+apuravkhare@users.noreply.github.com>
2022-03-14 10:50:40 -0400

    Commented the input field for demo

--- client/src/rooms/ScrollingCaption.js
+++ client/src/rooms/ScrollingCaption.js
@@ -52,8 +52,9 @@ const ScrollingCaption = ({currentUserId, displayCaptions, identifySpeakers,onEd
                             <span style={{float: "right",padding: "0.2em"}}>
                             <FontAwesomeIcon onClick = {() => onEditClick(caption["message"])} icon={faEdit} size="sm" />
                             </span>
-                            {/*<p style={{opacity: "100%"}}> {caption["message"]} </p>*/}
-                            <input   style={{opacity: "100%" }} type="text"  defaultValue = {caption["message"]}></input>
+                            <p style={{opacity: "100%"}}> {caption["message"]} </p>
+                            {/* Apurav: Commented below temporarily for demo */}
+                            {/* <input   style={{opacity: "100%" }} type="text"  defaultValue = {caption["message"]}></input> */}
                           </div>
                         </div>);
         }

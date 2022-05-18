58e74e3cc7c4286d64b804815a224709ee9a08e1
Apurav <33133457+apuravkhare@users.noreply.github.com>
2022-05-18 02:01:49 -0400

    Added new server config for demo

--- client/src/rooms/Room.js
+++ client/src/rooms/Room.js
@@ -105,12 +105,13 @@ const allowedFontSizes = ["small", "medium", "large", "x-large", "xx-large"];
  */
 
 const iceServersConfig = [
-    { "username": "1652378158:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "BL5Sw/0WtcdKeeQpz6+N0nFRu3A=", "urls": "turn:globalturn.subspace.com:3478?transport=udp" },
+    { urls: "stun:openrelay.metered.ca:80" },
+    { "username": "1652934409:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "4IF0sEFeSHltOVUTxEZo+R+XUEE=", "urls": "turn:globalturn.subspace.com:3478?transport=udp" },
+    // { "username": "1652934409:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "4IF0sEFeSHltOVUTxEZo+R+XUEE=", "urls": "turn:globalturn.subspace.com:3478?transport=tcp" },
+    // { "username": "1652934409:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "4IF0sEFeSHltOVUTxEZo+R+XUEE=", "urls": "turns:globalturn.subspace.com:5349?transport=udp" },
+    // { "username": "1652934409:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "4IF0sEFeSHltOVUTxEZo+R+XUEE=", "urls": "turns:globalturn.subspace.com:5349?transport=tcp" },
+    // { "username": "1652934409:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "4IF0sEFeSHltOVUTxEZo+R+XUEE=", "urls": "turns:globalturn.subspace.com:443?transport=tcp" },
     { "url": "turn:numb.viagenie.ca", "username": "ak2816@rit.edu", "credential": "jyBQUMNLuSdxR8n" }
-    // { "username": "1652378158:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "BL5Sw/0WtcdKeeQpz6+N0nFRu3A=", "urls": "turn:globalturn.subspace.com:3478?transport=tcp" },
-    // { "username": "1652378158:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "BL5Sw/0WtcdKeeQpz6+N0nFRu3A=", "urls": "turns:globalturn.subspace.com:5349?transport=udp" },
-    // { "username": "1652378158:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "BL5Sw/0WtcdKeeQpz6+N0nFRu3A=", "urls": "turns:globalturn.subspace.com:5349?transport=tcp" },
-    // { "username": "1652378158:prj_2Gb69igJRbn5vCZXXfNu8I", "credential": "BL5Sw/0WtcdKeeQpz6+N0nFRu3A=", "urls": "turns:globalturn.subspace.com:443?transport=tcp" }
 ];
 
 // const iceServersConfig = [

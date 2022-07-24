22b48cf98e2234f3491cbdcae423fa58bf43511f
rutvikpansare <rutvik.pansare@gmail.com>
2022-07-24 19:11:48 -0400

    added time for the hearing person

--- client/src/util/Time.js
+++ client/src/util/Time.js
@@ -4,8 +4,9 @@ import Clock from 'react-live-clock';
 
 const Time = (props) => {
   const isAdmin = props.parsed.admin
+  const genCaptions = props.parsed.genCaptions
   return (
-    <Clock style = {{visibility: isAdmin == 'true'? "visible": "hidden", color : 'white', margin: "5px"}}format={'HH:mm:ss'} ticking={true} timezone={'US/Eastern'} />
+    <Clock style = {{visibility: (isAdmin == 'true' || genCaptions == "true")? "visible": "hidden", color : 'white', margin: "5px"}}format={'HH:mm:ss'} ticking={true} timezone={'US/Eastern'} />
   )
 }
  
